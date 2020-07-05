5860b2b2bd5bb230ac4369582e8520705302dca6
seongsangCHO <secho@student.42seoul.kr>
2020-07-05 19:55:21 +0900

    delete detail.js file[secho]

--- back/models/db_config.js
+++ back/models/db_config.js
@@ -3,8 +3,8 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '5Eatramenmoto!',
-  database: 'yebalja'
+  password: '1234',
+  database: 'yebalja_db'
 })
 connection.connect((err)=>{
   if (err) throw err;
