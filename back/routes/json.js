3929a0e256b51a18dedbf428bf0576cd77f02f15
seongsangCHO <secho@student.42seoul.kr>
2020-07-06 11:47:47 +0900

    add program_table json page view

--- back/routes/json.js
+++ back/routes/json.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 let db = require('../models/db_config');
 
+
 router.get('/gisu', function(req, res, next) {
   db.query('SELECT * FROM gisu_table', (error, result)=>{
     if (error) throw error;
@@ -11,4 +12,10 @@ router.get('/gisu', function(req, res, next) {
   })
 });
 
+router.get('/program', function(req, res, next){
+  db.query('SELECT * FROM program_table', (error, result)=>{
+    if (error) throw error;
+    res.json(result);
+  })
+})
 module.exports = router;
