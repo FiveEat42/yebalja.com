8f33bb689fdb4c08ab1ee6770a52dea00536be52
365kim <365listener@gmail.com>
2020-07-09 01:21:01 +0900

    Add responsive footer

--- front/components/Footer.js
+++ front/components/Footer.js
@@ -1,19 +1,24 @@
-import './Footer.module.css'
+import styles from './Footer.module.css'
 
 const Footer = () => {
 
 	return (
-		<div className="footer">
-			<ul>
-				<li><a href="#">부트캠프란?</a></li>
-				<li><a href="#">예발자란?</a></li>
-				<li><a href="#">About 5E</a></li>
-				<li><a href="#">Email</a></li>
-				<li><a href="#">Github</a></li>
-				<li>
-				<p>👋</p>
-				</li>
-			</ul>
+		<div className={styles.footer}>
+			<div className={styles.copyright}>
+				Copyright 2020. 이름 All Rights Reserved.
+			</div>
+			<div className={styles.list}>
+				<ul>
+					<li><a href="#">부트캠프란?</a></li>
+					<li><a href="#">예발자란?</a></li>
+					<li><a href="#">About 5E</a></li>
+					<li><a href="#">Email</a></li>
+					<li><a href="#">Github</a></li>
+					<li>
+					<p>👋</p>
+					</li>
+				</ul>
+			</div>
 		</div>
 	);
 }
