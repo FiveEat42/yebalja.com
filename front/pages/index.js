1720a6ffb9afc75f3c717b34186b152e4fbae79a
Jeong-Ah Hong <jahong89@gmail.com>
2020-07-08 12:53:40 +0900

    modified index

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,12 +1,17 @@
 import Layout from '../components/Layout';
-import Jumbotron from '../components/Jumbotronself';
+import Jumbotron from '../components/Jumbotron'
+import Jumbo from '../components/JumbotronSelf';
 import Table from "../components/ResponsiveUsers";
 
 const Home = () => {
   return (
     <>
+      <Layout>
+        <div>Hello, Next!</div>
+        <Jumbotron />
+      </Layout>
       <img src={require('./yebalja.png')} height="60px" />
-      <Jumbotron />
+      <Jumbo />
 
 
       <Table />
