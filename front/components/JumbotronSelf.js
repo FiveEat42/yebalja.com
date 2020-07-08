4d25127b1f169c9a34fd903bd3bb270e8bc21889
Jeong-Ah Hong <jahong89@gmail.com>
2020-07-08 13:09:18 +0900

    changed jumbotron font

--- front/components/JumbotronSelf.js
+++ front/components/JumbotronSelf.js
@@ -9,7 +9,7 @@ const Jumbo = () => {
         <container className={styles.container}>
           <img className={styles.image} src={require('../pages/illustration.png')} width="600px"/>
           <div className={styles.text}>
-            <h1 className={styles.title}>누구나<br/>예발자 시절이 있었으니까,</h1>
+            <h1 className={styles.title}>누구나,<br/>예발자 시절이 있었으니까</h1><br/>
             <p className={styles.content}>예비개발자를 위한 코딩교육과정,<br/>부트캠프를 만나보세요.</p>
           </div>
         </container>
