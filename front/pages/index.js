c41d45bdfd3eb1113a6e07b83f3853efcbf243c0
Daehyun Lee <leehi0210@gmail.com>
2020-07-08 17:03:47 +0900

    Add YearlyCalendar

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,18 +1,33 @@
 import Layout from '../components/Layout';
 import Jumbotron from "../components/Jumbotron";
 import Table from "../components/Table";
+import TextLogo from '../components/TextLogo';
+import YearlyCalendar from '../components/YearlyCalendar';
 import Title from "../components/Title";
 import Subtitle from "../components/Subtitle";
 
 const Home = () => {
   return (
       <Layout>
+      	<TextLogo/>
         <Jumbotron />
         <Title> 내 일정에 맞는 부트캠프 찾기 </Title>
+      	<YearlyCalendar />
         <Subtitle> (연간일정 추가) </Subtitle>
         <Title> 한 눈에 보는 부트캠프 </Title>
         <Table />
       </Layout>
+=======
+
+
+const Home = () => {
+  return (
+    <>
+      {/*<Layout>*/}
+      {/*  <div>Hello, Next!</div>*/}
+      {/*</Layout>*/}
+    </>
+>>>>>>> Add YearlyCalendar
   );
 }
 
