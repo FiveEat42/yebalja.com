781282ea39b8874e054a16f84479215189ddae71
taelee <taelee@student.42seoul.kr>
2020-07-10 00:09:21 +0900

    refactor: change bcrypt-nodejs to bcyprt package

--- back/routes/register.js
+++ back/routes/register.js
@@ -1,16 +1,14 @@
-var express = require('express');
-var router = express.Router();
-var url = require('url');
-let db = require('../models/db_config');
-var bcrypt = require('bcrypt-nodejs');
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const db = require('../models/db_config')
 
 router.get('/', function(req, res, next) {
-    res.render('member/register');
-  });
-  
+  res.render('../views/register')
+});
 
-router.post('/', function(req, res, next){
-    //email, password, nickname을 받아와 DB내에 저장한다.
+router.post('/', async (req, res, next) => {
+	//email, password, nickname을 받아와 DB내에 저장한다.
     //저장될 때 성공, 실패를 맞추어야한다. 기준은 이메일의 중복, 닉네임의 중복.
 
     //이메일 또는 닉네임이 중복되면 반환하는 값을 console.log로 찍어본다
@@ -20,11 +18,16 @@ router.post('/', function(req, res, next){
     let body = req.body;
     console.log(body);
     //bcrypt를 이용해 패스워드 hash생성
-    bcrypt.hash(body.user_password, null ,null, function(err, hash){
-        console.log("hash data: "+hash);
-    });
-    //중복체크해야함 이건 내일하자..
-    res.redirect('/api/admin');
+  const { nickname, email, password } = req.body;
+  console.log(typeof(email))
+  db.query(`SELECT * FROM member_table WHERE member_login_email = '${email}'`,(error, result) => {
+    if (error) throw error;
+    console.log('query result', result);
+  });
+  bcrypt.hash(req.body.password, 10, (err, hash) => {
+  })
+  res.redirect('/api/register')
+
 })
 
 module.exports = router;
