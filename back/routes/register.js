c83a169f31c20bfaea55d109432fcf3be5da94e2
taelee <taelee@student.42seoul.kr>
2020-07-13 07:51:24 +0900

    add comment on register file, remove box highlight on register pug file

--- back/routes/register.js
+++ back/routes/register.js
@@ -7,34 +7,32 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', (req, res, next) => {
-    //email, password, nickname을 받아와 DB내에 저장한다.
-    //저장될 때 성공, 실패를 맞추어야한다. 기준은 이메일의 중복, 닉네임의 중복.
-    //이메일 또는 닉네임이 중복되면 반환하는 값을 console.log로 찍어본다
-    //찍힌 값을 기준으로 성공, 실패를 구분하며, 성공시 api/login화면으로 리다이렉트,
-    //실패시 console.log로 이메일, 닉네임 중복여부를 보여준다.
-    //bcrypt를 이용해 패스워드 hash생성
-
   const { nickname, email, password, passwordCheck } = req.body;
+  //처음에 패스워드와 패스워드 확인의 비밀번호가 서로 일치하는 검사해서 일치할때만 다음단계로 넘어갑니다.
   if (password !== passwordCheck) {
     console.log('비밀번호 불일치');
     res.redirect('/api/register');
   } else {
+    //입력받은 email과 db에 중복되는 데이터가 있는지 검사합니다.
     db.query(`SELECT * FROM member_table WHERE member_login_email = '${email}'`,
       (error, result) => {
         if (error) throw error;
         if (!result[0]) {
+          //입력받은 nickname과 db에 중복되는 데이터가 있는지 검사합니다.
           db.query(`SELECT * FROM member_table WHERE member_login_nickname = '${nickname}'`,
             (err1, result1) => {
               if (err1) throw err1;
               if (!result1[0]) {
+                //입력받은 password를 암호화합니다.
                 bcrypt.hash(password, 10, (err, hash) => {
+                  //위의 모든 테스트를 통과한 경우 정상데이터로 판단해서 DB에 넣습니다.
                   db.query(`
                     INSERT INTO member_table(member_login_nickname, member_login_pw, member_login_email)
                     VALUES (?, ?, ?)`,
                     [nickname, hash, email],
                     (err2, result2) => {
                       if (err2) throw err2;
-                      console.log('result', result2);
+                      console.log('회원가입 성공', result2);
                     })
                 })
                 res.redirect('/api/login')
