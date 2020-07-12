a97781611d1ca7d650f08f7fdb537b355642e1c6
365kim <365listener@gmail.com>
2020-07-12 15:06:26 +0900

    Add active class in nav item when selected

--- front/components/NavBar.js
+++ front/components/NavBar.js
@@ -3,21 +3,51 @@ import Link from 'next/link';
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import classNames from 'classnames';
+import { useRouter } from "next/router";
+
 
 const NavBar = () => {
+
+    const router = useRouter();
+
     return (
         <div>
-            <Navbar collapseOnSelect expand="lg" variant="dark" className={styles.navbarBg}>
+            <Navbar fixed="top" collapseOnSelect expand="lg" variant="dark" className={styles.navbarBg}>
                 <Navbar.Brand></Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mr-auto">
-                        <Link href="/ftseoul"><a className="nav-link">42서울</a></Link>
-                        <Link href="/ssafy"><a className="nav-link">SSAFY</a></Link>
-                        <Link href="/boostcamp"><a className="nav-link">부스트캠프</a></Link>
-                        <Link href="/soma"><a className="nav-link">SOMA</a></Link>
-                        <Link href="/woowa"><a className="nav-link">우아한테크코스</a></Link>
-                        <Link href="/likelion"><a className="nav-link">멋쟁이사자처럼</a></Link>
+                        <Link href="/ftseoul">
+                            <a className={classNames({["nav-link"]: true,
+                                [router.pathname == "/ftseoul" ? "active" : ""]: true})}>
+                                42서울</a>
+                        </Link>
+                        <Link href="/ssafy">
+                            <a className={classNames({["nav-link"]: true,
+                                [router.pathname == "/ssafy" ? "active" : ""]: true})}>
+                                SSAFY</a>
+                        </Link>
+                        <Link href="/boostcamp">
+                            <a className={classNames({["nav-link"]: true,
+                                [router.pathname == "/boostcamp" ? "active" : ""]: true})}>
+                                부스트캠프</a>
+                        </Link>
+                        <Link href="/soma">
+                            <a className={classNames({["nav-link"]: true,
+                                [router.pathname == "/soma" ? "active" : ""]: true})}>
+                                SOMA</a>
+                        </Link>
+                        <Link href="/woowa">
+                            <a className={classNames({["nav-link"]: true,
+                                [router.pathname == "/woowa" ? "active" : ""]: true})}>
+                                우아한테크코스</a>
+                        </Link>
+                        <Link href="/likelion">
+                            <a className={classNames({["nav-link"]: true,
+                                [router.pathname == "/likelion" ? "active" : ""]: true})}>
+                                멋쟁이사자처럼</a>
+                        </Link>
                     </Nav>
                     <Nav>
                         <Nav.Link href="/sign-in">로그인</Nav.Link>
