1aacde3e02635b3cded01797d799c7ffabcf7334
Daehyun Lee <leehi0210@gmail.com>
2020-07-15 20:11:35 +0900

    Add review links

--- front/components/Review.js
+++ front/components/Review.js
@@ -6,7 +6,6 @@ import Link from 'next/link';
 export default function Review() {
     return (
       <>
-
         <nav className={classNames({[styles.menu]: true, [styles.menu__adsila]: true})}>
           <a className={styles.menu__item} href="#">
             <span className={styles.menu__item_name}>Artists</span>
@@ -27,14 +26,6 @@ export default function Review() {
           <a className={styles.menu__item} href="#">
             <span className={styles.menu__item_name}>The Gardens</span>
             <span className={styles.menu__item_label}>Get to know our eco village</span>
-          </a>
-          <a className={styles.menu__item} href="#">
-            <span className={styles.menu__item_name}>Buy Tickets</span>
-            <span className={styles.menu__item_label}>Purchase event tickets online</span>
-          </a>
-          <a className={styles.menu__item} href="#">
-            <span className={styles.menu__item_name}>Contact</span>
-            <span className={styles.menu__item_label}>Get in touch and find us</span>
           </a>
         </nav>
       </>
