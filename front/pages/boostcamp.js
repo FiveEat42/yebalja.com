44b957cef8a96f622fd3777015dba07beec550e8
Daehyun Lee <leehi0210@gmail.com>
2020-07-16 17:36:44 +0900

    Modify Yearly

--- front/pages/boostcamp.js
+++ front/pages/boostcamp.js
@@ -3,21 +3,22 @@ import Header from "../components/Header";
 import React from "react";
 import Review from "../components/Review";
 
+
 const Boostcamp = () => {
     return (
         <Layout>
             <Header>
-                <h2>네이버 부스트캠프</h2>
+                <h1>네이버 부스트캠프</h1>
                 <p>개발자의 지속 가능한 성장을 추구하는 학습 커뮤니티</p>
             </Header>
 
-          <Timeline />
+            {/* <Timeline /> */}
             <Header>
-                <h4>상세설명</h4>
+                <h3>상세설명</h3>
                 <p>추가예정</p>
             </Header>
             <Header>
-                <h4>부트캠프 참여후기</h4>
+                <h3>부트캠프 참여후기</h3>
                 <p>솔직담백한 이야기를 공유합니다</p>
             </Header>
             <Review></Review> 
