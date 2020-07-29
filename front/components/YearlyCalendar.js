a73b2004bd3550afa0ec999671fa01b920b58942
Daehyun Lee <leehi0210@gmail.com>
2020-07-29 19:49:04 +0900

    Using props in React style

--- front/components/YearlyCalendar.js
+++ front/components/YearlyCalendar.js
@@ -4,6 +4,97 @@ import classNames from 'classnames';
 import Header from "./Header";
 import CTA from "./CTA";
 
+export function ProgramList() {
+    const ProgramData = [
+        {
+            program: "42 Seoul",
+            step: [
+                {
+                    title:  "지원",
+                    date: "상시모집",
+                    gridColumn: "10/234",
+                },
+                {
+                    title: "라피신",
+                    date: "1/23 ~ 2/16",
+                    gridColumn: "10/234",
+                },
+                {
+                    title: "교육",
+                    date: "2/27 ~ 최대 2년",
+                    gridColumn: "10/234",
+                }
+            ],
+        },{
+            program: "2기",
+            step: [
+                {
+                    title:  "라피신",
+                    date: "2-1기 : 6/29 ~ 7/24",
+                    gridColumn: "10/234",
+                },
+                {
+                    title: "라피신",
+                    date: "1/23 ~ 2/16",
+                    gridColumn: "10/234",
+                },
+                {
+                    title: "교육",
+                    date: "3/27 ~ 최대 2년",
+                    gridColumn: "10/234",
+                }
+            ],
+        },
+    ]
+    return (
+        <>
+            {ProgramData.map((v) => <StepList data={v}/>)}
+        </>
+    )
+}
+
+export function StepList({data}) {
+    return (
+        <>
+            <div className={styles.gantt__row}>
+                <div className={styles.gantt__row__first}>
+                    <span>{data.program}</span>
+
+                </div>
+                <ul className={styles.gantt__row__bars}>
+                    {data.step.map((v) => <StepListItem data={v}/>)}
+                </ul>
+            </div>
+        </>
+    )
+}
+
+export function StepListItem({data}) {
+    return (
+        <>
+            <li className={classNames({[styles.calendar_entry]: true, [styles.oversize_list]: true})} 
+            style={{ gridColumn: data.gridColumn, backgroundColor: `#4DF0FF`, zIndex: `3`}}>
+                <span className={styles.calendar_entry__date}>{data.date}</span>
+                <span className={styles.calendar_entry__title}>{data.title}</span>
+            </li>
+        </>
+    )
+}
+
+export function YearByWeekLines() {
+    
+   return (
+        <>
+            <div className={classNames({[styles.gantt__row]: true, [styles.gantt__row__months]: true})}>
+                <div className={styles.gantt__row__first}></div>
+                {Array(12).fill('').map((v, idx) => <span>{idx+1}월</span>)}
+            </div>
+            <div className={classNames({[styles.gantt__row]: true, [styles.gantt__row__lines]: true})}>
+                {Array(54).fill('').map((v, idx) => <span></span>)}
+            </div>
+        </>
+    )
+}
 
 export default function YearlyCalendar() {
 
@@ -12,45 +103,15 @@ export default function YearlyCalendar() {
       <Header className={styles.webheader}>
         <h1>2020년 부트캠프</h1>
         <p className={styles.websubtitle}>무료 부트캠프 일정을 한 눈에 확인하세요</p>
-        {/*<p className={styles.mobilesubtitle}>무료 부트캠프 연간 일정은 웹사이트 혹은 가로모드에서 지원됩니다</p>*/}
       </Header>
-
       <div className={classNames({[styles.wrapper]: true, [styles.landscape_only]: true})}>
-
         <CTA />
-
-
           <div className={styles.gantt}>
-              <div className={classNames({[styles.gantt__row]: true, [styles.gantt__row__months]: true})}>
-                      <div className={styles.gantt__row__first}></div>
-                      <span>1월</span><span>2월</span><span>3월</span>
-                      <span>4월</span><span>5월</span><span>6월</span>
-                      <span>7월</span><span>8월</span><span>9월</span>
-                      <span>10월</span><span>11월</span><span>12월</span>
-              </div>
-              <div className={classNames({[styles.gantt__row]: true, [styles.gantt__row__lines]: true})}>
-                      <span></span><span></span><span></span>
-                      <span></span><span></span><span></span>
-                      <span></span><span></span><span></span>
-                      <span></span><span></span><span></span>
-                      <span></span><span></span><span></span>
-                      <span></span><span></span><span></span>
-                      <span></span><span></span><span></span>
-                      <span></span><span></span><span></span>
-                      <span></span><span></span><span></span>
-                      <span></span><span></span><span></span>
-                      <span></span><span></span><span></span>
-                      <span></span><span></span><span></span>
-                      <span></span><span></span><span></span>
-                      <span></span><span></span><span></span>
-                      <span></span><span></span><span></span>
-                      <span></span><span></span><span></span>
-                      <span></span><span></span><span></span>
-                      <span></span><span></span><span></span>
-              </div>
-              <div className={styles.gantt__row}>
+              <YearByWeekLines />
+              <ProgramList />
+              {/* <div className={styles.gantt__row}>
                   <div className={styles.gantt__row__first}>
-                      <span>42 Seoul</span> <br></br><br></br><span className={styles.gisu}>1기</span>
+                      <span>42 Seoul<br></br><br></br>1기</span>
                   </div>
                   <ul className={styles.gantt__row__bars}>
                       <li className={classNames({[styles.calendar_entry]: true, [styles.oversize_list]: true})} style={{ gridColumn: `1/366`, backgroundColor: `#4DF0FF`, zIndex: `3`}}>
@@ -60,12 +121,10 @@ export default function YearlyCalendar() {
                       <li className={styles.calendar_entry} style={{ gridColumn: `23/46`, backgroundColor: `#42A7F1`, zIndex: `2` }}>
                           <span className={styles.calendar_entry__date}>1/23 ~ 2/16</span>
                           <span className={styles.calendar_entry__title}>라피신</span>
-                          {/* <span className={styles.calendar_entry__details}><a>자세히보기</a></span> */}
                       </li>
                       <li className={classNames({[styles.calendar_entry]: true, [styles.oversize_list]: true, [styles.next_year]: true})} style={{ gridColumn: `57/380`, backgroundColor: `#CBDFFD`, zIndex: `1`}}>
                           <span className={styles.calendar_entry__date}>2/27 ~ 최대 2년</span>
                           <span className={styles.calendar_entry__title}>교육</span>
-                          {/* <span className={styles.calendar_entry__details}><a>최대 2년</a></span> */}
                       </li>
                   </ul>
               </div>
@@ -115,11 +174,9 @@ export default function YearlyCalendar() {
                       <li className={styles.calendar_entry} style={{ gridColumn: `129/149`, backgroundColor: `#4DF0FF`, zIndex: `3` }}>
                           <span className={styles.calendar_entry__date}>5/11 ~ 5/25</span>
                           <span className={styles.calendar_entry__title}>지원</span>
-                          <span className={styles.calendar_entry__details}><a>4기</a></span>
                       </li>
                       <li className={styles.calendar_entry} style={{ gridColumn: `155/175`, backgroundColor: `#54c6f9`, zIndex: `2` }}>
-                          <span className={styles.calendar_entry__date}>SW적성진단 : 6/06</span>
-                          <span className={styles.calendar_entry__date}>현장 인터뷰 : 6/17 ~ 6/19</span>
+                          <span className={styles.calendar_entry__date}>SW적성진단 : 6/06 <br/> 현장 인터뷰 : 6/17 ~ 6/19</span>
                           <span className={styles.calendar_entry__title}>선발</span>
                       </li>
                       <li className={classNames({[styles.calendar_entry]: true, [styles.oversize_list]: true, [styles.next_year]: true})} style={{ gridColumn: `187/380`, backgroundColor: `#CBDFFD`, zIndex: `1` }}>
@@ -140,15 +197,12 @@ export default function YearlyCalendar() {
                           <span className={styles.calendar_entry__details}><a>4기</a></span>
                       </li>
                       <li className={styles.calendar_entry} style={{ gridColumn: `186/207`, backgroundColor: `#54c6f9`, zIndex: `2` }}>
-                          <span className={styles.calendar_entry__date}>1차 코딩테스트 : 7/04</span>
-                          <span className={styles.calendar_entry__date}>2차 코딩테스트 : 7/20</span>
+                          <span className={styles.calendar_entry__date}>1차 코딩테스트 : 7/04 <br/> 2차 코딩테스트 : 7/20</span>
                           <span className={styles.calendar_entry__title}>선발</span>
-                          <span className={styles.calendar_entry__details}><a>자세히보기</a></span>
                       </li>
                       <li className={styles.calendar_entry} style={{ gridColumn: `207/237`, backgroundColor: `#42A7F1`, zIndex: `1` }}>
                           <span className={styles.calendar_entry__date}>7/27 ~ 8/21</span>
                           <span className={styles.calendar_entry__title}>챌린지</span>
-                          <span className={styles.calendar_entry__details}><a>자세히보기</a></span>
                       </li>
                       <li className={classNames({[styles.calendar_entry]: true, [styles.oversize_list]: true})} style={{ gridColumn: `237/351`, backgroundColor: `#CBDFFD` }}>
                           <span className={styles.calendar_entry__date}>8/31 ~ 12/21</span>
@@ -164,12 +218,9 @@ export default function YearlyCalendar() {
                       <li className={styles.calendar_entry} style={{ gridColumn: `22/51`, backgroundColor: `#4DF0FF`, zIndex: `3` }}>
                           <span className={styles.calendar_entry__date}>1/22 ~ 2/21</span>
                           <span className={styles.calendar_entry__title}>지원</span>
-                          <span className={styles.calendar_entry__details}><a>4기</a></span>
                       </li>
                       <li className={styles.calendar_entry} style={{ gridColumn: `74/129`, backgroundColor: `#54c6f9`, zIndex: `2` }}>
-                          <span className={styles.calendar_entry__date}>1차 코딩테스트 : 3/14</span>
-                          <span className={styles.calendar_entry__date}>2차 코딩테스트 : 4/26</span>
-                          <span className={styles.calendar_entry__date}>심층면접 : 5/08~09</span>
+                          <span className={styles.calendar_entry__date}>1차 코딩테스트 : 3/14 <br/>2차 코딩테스트 : 4/26 <br/>심층면접 : 5/08~09</span>
                           <span className={styles.calendar_entry__title}>선발</span>
                       </li>
                       <li className={classNames({[styles.calendar_entry]: true, [styles.oversize_list]: true})} style={{ gridColumn: `153/365`, backgroundColor: `#CBDFFD` }}>
@@ -197,15 +248,13 @@ export default function YearlyCalendar() {
                       <li className={styles.calendar_entry} style={{ gridColumn: `77/107`, backgroundColor: `#4DF0FF`, zIndex: `1` }}>
                           <span className={styles.calendar_entry__date}>3/17 ~ 대학별 상이 </span>
                           <span className={styles.calendar_entry__title}>지원</span>
-                          <span className={styles.calendar_entry__details}><a>8기</a></span>
                       </li>
                       <li className={classNames({[styles.calendar_entry]: true, [styles.oversize_list]: true})} style={{ gridColumn: `107/270`, backgroundColor: `#CBDFFD` }}>
                           <span className={styles.calendar_entry__date}>4월 ~ 10월</span>
                           <span className={styles.calendar_entry__title}>교육</span>
-                          <span className={styles.calendar_entry__details}><a></a></span>
                       </li>
                   </ul>
-              </div>
+              </div> */}
           </div>
       </div>
   </>
