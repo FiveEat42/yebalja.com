9bf4fa50667a2c585679e6e96510105bcd39eb6c
365kim <365listener@gmail.com>
2020-07-30 22:46:45 +0900

    Clean up

--- front/components/Layout.js
+++ front/components/Layout.js
@@ -1,23 +1,24 @@
+import React from 'react';
 import PropTypes  from 'prop-types';
 import Footer from "./Footer";
 import TextLogo from "./TextLogo";
 import NavBar from "./NavBar"
 
 const Layout = ({children}) => {
-    return (
-        <div>
-            <div>
-                <TextLogo/>
-                <NavBar />
-                {children}
-                <Footer />
-            </div>
-        </div>
-    )
+	return (
+		<div>
+			<div>
+				<TextLogo/>
+				<NavBar />
+					{children}
+				<Footer />
+			</div>
+		</div>
+	)
 }
 
 Layout.propTypes = {
-    children: PropTypes.node.isRequired,
+	children: PropTypes.node.isRequired,
 };
 
 export default Layout;
