c3f053f5551a163a4885ce1bf596ede6360141fd
jehong <jehong@student.42seoul.com>
2020-08-01 10:22:24 +0900

    modify cta text and modify dummy structure

--- front/components/TimelineList.js
+++ front/components/TimelineList.js
@@ -1,67 +1,68 @@
 import TimelineListItem from "./TimeLineListItem";
 
 export default function TimelineList({program}) {
-	const timeline = {
-		boostcamp: [
-			{
-				id: `one`,
-				card_style: 'card_now',
-				status: 'status',
-				name: '접수기간',
-				description: '4기 서류지원',
-				startdate: '2020-06-01',
-				date: '6.1 11:00 ~ 6.30 23:59',
-			},
-			{
-				id: `two`,
-				card_style: 'card_now',
-				status: 'status',
-				name: '1차',
-				description: '온라인 코딩테스트',
-				startdate: '2020-07-04',
-				date: '7.4',
-			},
-			{
-				id: `three`,
-				card_style: 'card_now',
-				status: 'status',
-				name: '2차',
-				description: '온라인 코딩테스트',
-				startdate: '2020-07-11',
-				date: '7.11',
-			},
-			{
-				id: `four`,
-				card_style: 'card_now',
-				status: 'status',
-				name: '최종 합격자 발표',
-				description: '',
-				startdate: '2020-07-20',
-				date: '7.20',
-			},
-			{
-				id: `five`,
-				card_style: 'card_edu',
-				status: 'status_edu',
-				name: '부스트캠프 챌린지',
-				description: '수료자 선발',
-				startdate: '2020-07-27',
-				date: '7.27 ~ 8.21',
-			},
-			{
-				id: `six`,
-				type: 'edu',
-				name: '부스트캠프 멤버',
-				description: '챌린지 과정 수료자',
-				startdate: '2020-08-31',
-				date: '8.31 ~ 12.21',
-			},
-		]
-	}
+  const timeline = {
+    boostcamp: [
+      {
+        id: `1`,
+        type: 'now',
+        name: '접수기간',
+        description: '4기 서류지원',
+        startdate: '2020-06-01',
+        enddate: '2020-06-30',
+        period: '6.1 11:00 ~ 6.30 23:59',
+      },
+      {
+        id: `2`,
+        type: 'now',
+        name: '1차',
+        description: '온라인 코딩테스트',
+        startdate: '2020-07-04',
+        enddate: '2020-07-04',
+        period: '7.4',
+      },
+      {
+        id: `3`,
+          type: 'now',
+        name: '2차',
+        description: '온라인 코딩테스트',
+        startdate: '2020-07-11',
+        enddate: '2020-07-11',
+        period: '7.11',
+      },
+      {
+        id: `4`,
+          type: 'now',
+        name: '최종 합격자 발표',
+        description: '',
+        startdate: '2020-07-20',
+        enddate: '2020-07-20',
+        period: '7.20',
+      },
+      {
+        id: `5`,
+        type: 'edu',
+        name: '부스트캠프 챌린지',
+        description: '수료자 선발',
+        startdate: '2020-07-27',
+        enddate: '2020-08-21',
+        period: '7.27 ~ 8.21',
+      },
+      {
+        id: `6`,
+        type: 'edu',
+        name: '부스트캠프 멤버',
+        description: '챌린지 과정 수료자',
+        startdate: '2020-08-31',
+        enddate: '2020-12-21',
+        period: '8.31 ~ 12.21',
+      },
+    ]
+}
 
-	return (
-		<>
-		{timeline[program].map((v, index) => <TimelineListItem data={v} key={index} />)}
-	</>
+  return (
+    <>
+    {timeline[program].map((v, index) => <TimelineListItem data={v} key={index} />)}
+  </>
 )
 }
--- front/components/TimelineListItem.js
+++ front/components/TimelineListItem.js
@@ -3,27 +3,32 @@ import TimelineList from './TimelineList';
 import classNames from 'classnames';
 
 export default function TimelineListItem({data}) {
-
-	const currentdate = new Date();												{/* new Date()함수는 현재 날짜를 생성 */}
-	const targetdate = new Date(data.startdate);			{/* new Date(`MM/DD/YY`)는 입력한 날짜를 생성 */}
-  let difference =  Math.floor((targetdate - currentdate)/(1000 * 60 * 60 * 24)) + 1;		{/* getTime()함수는 날짜를 총 시간으로 표기 */}
-  let timeLeft;  																				{/* 남은 날짜를 담을 변수 */}
-	let card_status = 'card_'+data.type;
-	let status = 'status_'+data.type;
-	let dot = 'dot_'+data.type;
 	let updown;
+	let startdateLeft = Math.floor((new Date(data.startdate).setHours(9) - new Date().setHours(9)) / (1000 * 60 * 60 * 24)) + 1;
+	let enddateLeft = Math.floor((new Date(data.enddate).setHours(9) - new Date().setHours(9)) / (1000 * 60 * 60 * 24)) + 1;
+	let status_content;				{/* 모집현황 멘트 (D-Day, 진행중, 마감) */}
+	{/* 카드, 모집현황, 점의 클래스네임이 모집(now)인지 교육(edu)인지 알려주기 */}
+	let card_type = 'card_' + data.type;
+	let status = 'status_' + data.type;
+	let dot = 'dot_' + data.type;
 
+	{/* 카드가 위/아래 중 어디에 위치할지 */}
 	if (data.id % 2 == 1) {
-		updown = 'up';
+		updown = 'up_' + data.type;
 	} else {
-		updown = 'down';
+		updown = 'down_' + data.type;
 	}
 
-  if (difference >= 0) {
-    timeLeft = `D-${difference}`;			{/* 시간의 차를 반내림하면 며칠 차이 나는지 구할 수 있다 */}
-  } else {
-    timeLeft = '마감'; 						{/* 지난 날은 무조건 -1로 표시하기로 정함 */}
-    card_status = 'card_end';
+	{/* 시작날짜 기준으로 DDAY 산정 && 마감날짜 기준으로 진행중/마감 알려주 */}
+	if (startdateLeft > 0) {
+		status_content = `D-${startdateLeft}`;
+	} else if (startdateLeft == 0) {
+		status_content = 'D-Day';
+	} else if (enddateLeft >= 0) {
+		status_content = '진행중';
+	} else {
+    status_content = '마감';
+    card_type = 'card_end';
   	status = 'status_end';
   	dot = 'dot_end';
   }
@@ -33,14 +38,14 @@ export default function TimelineListItem({data}) {
 		return (
 				<div className={styles.box}>
 					<div className={styles[dot]}></div>
-					<div className={classNames({[styles.card]: true, [styles[card_status]]: true, [styles[updown]]: true})}>
+					<div className={classNames({[styles[card_type]]: true, [styles[updown]]: true})}>
 						{data.name}
 						<span className={styles[status]}>
-							{timeLeft}
+							{status_content}
 						</span>
 						<div className={styles.content}>
 							{data.description} <br/>
-							{data.date}
+							`{data.startdate} ~ {data.enddate}`
 						</div>
 					</div>
 				</div>
