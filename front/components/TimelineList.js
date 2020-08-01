55ceb825d43c8d1805a29eb8e3805860e12a3407
jehong <jehong@student.42seoul.com>
2020-08-02 05:42:51 +0900

    add dummydata for all programs' timelines and headers

--- front/components/TimelineList.js
+++ front/components/TimelineList.js
@@ -4,14 +4,14 @@ export default function TimelineList({program}) {
   const timeline = {
       ftseoul: [
       {
-        edu: `true`,
+        edu: `false`,
         name: '2기 1차 라피신',
         description: '본과정 선발을 위한 집중 교육 과정',
         startdate: '2020-06-29',
         enddate: '2020-07-24',
       },
       {
-        edu: 'true',
+        edu: 'false',
         name: '2기 2차 라피신',
         description: '본과정 선발을 위한 집중 교육 과정',
         startdate: '2020-07-27',
@@ -25,15 +25,14 @@ export default function TimelineList({program}) {
         enddate: '2022-09-01',
       },
       {
-        edu: 'true',
+        edu: 'false',
         name: '3기 1차 라피신',
         description: '본과정 선발을 위한 집중 교육 과정',
         startdate: '2020-08-31',
         enddate: '2020-09-25',
       },
       {
-        id: `5`,
-        edu: 'true',
+        edu: 'false',
         name: '3기 2차 라피신',
         description: '본과정 선발을 위한 집중 교육 과정',
         startdate: '2020-10-05',
@@ -45,6 +44,36 @@ export default function TimelineList({program}) {
         description: '3기 1차와 2차 본과정 시작',
         startdate: '2020-11-15',
         enddate: '2022-11-15',
+      },
+    ],
+    ssafy: [
+      {
+        edu: `false`,
+        name: '접수기간',
+        description: '4기 서류 접수',
+        startdate: '2020-05-11',
+        enddate: '2020-05-25',
+      },
+      {
+        edu: 'false',
+        name: '1차 선발과정',
+        description: '온라인 SW 적성진단',
+        startdate: '2020-06-06',
+        enddate: '2020-06-06',
+      },
+      {
+        edu: 'false',
+        name: '2차',
+        description: '현장 인터뷰',
+        startdate: '2020-06-17',
+        enddate: '2022-06-19',
+      },
+      {
+        edu: 'true',
+        name: '교육 시작',
+        description: '입과 및 교육',
+        startdate: '2020-07-07',
+        enddate: '2020-08-07',
       },
     ],
     boostcamp: [
@@ -77,7 +106,7 @@ export default function TimelineList({program}) {
         enddate: '2020-07-20',
       },
       {
-        edu: 'true',
+        edu: 'false',
         name: '부스트캠프 챌린지',
         description: '수료자 중에서 선발',
         startdate: '2020-07-27',
@@ -89,6 +118,68 @@ export default function TimelineList({program}) {
         description: '선발자만 부스트캠프 멤버쉽 참가 가능',
         startdate: '2020-08-31',
         enddate: '2020-12-21',
+      },
+    ],
+    soma: [
+      {
+        edu: `false`,
+        name: '접수기간',
+        description: '11기 서류 접수',
+        startdate: '2020-01-22',
+        enddate: '2020-02-21',
+      },
+      {
+        edu: `false`,
+        name: '1차 전형',
+        description: '온라인 코딩테스트',
+        startdate: '2020-03-14',
+        enddate: '2020-03-14',
+      },
+      {
+        edu: `false`,
+        name: '인적성 검사',
+        description: '온라인 AI 인적성 검사',
+        startdate: '2020-04-08',
+        enddate: '2020-05-06',
+      },
+      {
+        edu: `false`,
+        name: '2차 전형',
+        description: '온라인 코딩테스트',
+        startdate: '2020-04-26',
+        enddate: '2020-04-26',
+      },
+      {
+        edu: `false`,
+        name: '심층면접',
+        description: '오프라인 심층면접',
+        startdate: '2020-05-08',
+        enddate: '2020-05-09',
+      },
+      {
+        edu: `true`,
+        name: '본과정',
+        description: '6개월 연수과정',
+        startdate: '2020-06-01',
+        enddate: '2020-11-30',
+      },
+    ],
+    woowa: [
+      {
+        edu: 'false',
+        name: '2기 선발 마감',
+        description: '3기 2021년 시작 예정',
+        startdate: '2019-10-25',
+        enddate: '2020-1-03',
+      },
+    ],
+    likelion: [
+      {
+        edu: `false`,
+        name: '2020년 선발 마감',
+        description: '선발절차는 대학별 상이',
+        startdate: '2020-03-17',
+        enddate: '2020-03-17',
       },
     ]
 }
--- front/pages/woowa.js
+++ front/pages/woowa.js
@@ -1,27 +1,13 @@
 import Layout from "../components/Layout";
 import Header from "../components/Header";
 import React from "react";
+import Timeline from "../components/Timeline";
 
 const Woowa = () => {
     return (
-        <Layout>
-            <Header>
-                <h2>우아한 테크코스</h2>
-                <p>부트캠프 상세페이지안을 짜봅시다</p>
-            </Header>
-            <Header>
-                <h4>타임라인</h4>
-                <p>추가예정</p>
-            </Header>
-            <Header>
-                <h4>상세설명</h4>
-                <p>추가예정</p>
-            </Header>
-            <Header>
-                <h4>프로그램 참여후기</h4>
-                <p>추가예정</p>
-            </Header>
-        </Layout>
+    <Layout>
+      <Timeline program="woowa"/>
+    </Layout>
     );
 }
 
