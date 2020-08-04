f277909d1b81a87bb261f070a3218d59e8b2f5ba
taelee <taelee@student.42seoul.kr>
2020-08-04 20:03:45 +0900

    Add table component json from DB

--- back/config/db_config.js
+++ back/config/db_config.js
@@ -4,9 +4,9 @@ require('dotenv').config();
 const connection = mysql.createConnection({
   host: '34.64.143.77',
   port: 3306,
-  user: 'secho',
+  user: 'root',
   password: process.env.DB_PASSWORD,
-  database: 'yebalja_db'
+  database: 'yebalja_db2'
 })
 connection.connect((err)=>{
   if (err) throw err;
--- back/routes/json.js
+++ back/routes/json.js
@@ -18,4 +18,31 @@ router.get('/program', function(req, res, next){
     res.json(result);
   })
 })
+
+router.get('/table', (req, res) => {
+  db.query(`select
+'program', title,
+  'link', link,
+  'number_of_recruits', personnel,
+  'edu_period', edu_period,
+  'grant', benefit,
+  'aptitude', badge_aptitude,
+  'coding', badge_coding,
+  'interview', badge_interview,
+  'tryout', badge_tryout
+from programs;`, (error, result) => {
+    let arr = [];
+    if (error) throw error;
+    // console.log(result);
+    result.map((v) => {
+      if (v.link != '/likelion'){
+        arr.push(v);
+      }
+    })
+    console.log(arr)
+    res.json(arr);
+  })
+});
+
 module.exports = router;
+
