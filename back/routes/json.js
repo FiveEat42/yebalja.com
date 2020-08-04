202828fa09c07861b43f44ea218702d4fd1510c5
taelee <taelee@student.42seoul.kr>
2020-08-04 20:26:57 +0900

    Add Timeline component json from DB

--- back/routes/json.js
+++ back/routes/json.js
@@ -21,28 +21,46 @@ router.get('/program', function(req, res, next){
 
 router.get('/table', (req, res) => {
   db.query(`select
-'program', title,
-  'link', link,
-  'number_of_recruits', personnel,
-  'edu_period', edu_period,
-  'grant', benefit,
-  'aptitude', badge_aptitude,
-  'coding', badge_coding,
-  'interview', badge_interview,
-  'tryout', badge_tryout
-from programs;`, (error, result) => {
-    let arr = [];
-    if (error) throw error;
-    // console.log(result);
-    result.map((v) => {
-      if (v.link != '/likelion'){
-        arr.push(v);
-      }
-    })
-    console.log(arr)
-    res.json(arr);
+    'program', title,
+    'link', link,
+    'number_of_recruits', personnel,
+    'edu_period', edu_period,
+    'grant', benefit,
+    'aptitude', badge_aptitude,
+    'coding', badge_coding,
+    'interview', badge_interview,
+    'tryout', badge_tryout
+    from programs;`, (error, result) => {
+      let arr = [];
+      if (error) throw error;
+      result.map((v) => {
+        if (v.link != '/likelion'){
+          arr.push(v);
+        }
+      })
+      res.json(arr);
   })
 });
+router.get('/timeline', (req, res) => {
+  db.query(`select substring(link, 2), json_array((
+    json_object(
+            'heading', heading,
+            'subheading', subheading
+        )
+    ))
+from programs;`, (error, result) => {
+    if (error) throw result;
+    let obj = {};
+    result.map(v => {
+      let dbString = Object.values(v)
+      let key = dbString[0];
+      let value = JSON.parse(dbString[1]);
+      obj[key] = value;
+    })
+    console.log('obj',obj);
+    res.json(obj);
+  })
+})
 
 module.exports = router;
 
