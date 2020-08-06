e55969b990e11d518ceef4c09800b37b34da962a
taelee <taelee@student.42seoul.kr>
2020-08-06 15:49:42 +0900

    Add Timelinelist component json from DB and Add comment in json.js

--- back/routes/json.js
+++ back/routes/json.js
@@ -37,6 +37,7 @@ router.get('/table', (req, res) => {
       result.map((v) => {
         let parsed =JSON.parse(Object.values(v));
         console.log(parsed);
+        //likelion은 테이블에 안쓰이므로 삭제
         if (parsed.link !== '/likelion'){
           arr.push(parsed);
         }
@@ -45,76 +46,56 @@ router.get('/table', (req, res) => {
       res.json(arr);
   })
 });
+
 router.get('/timeline', (req, res) => {
-  db.query(`select substring(link, 2), json_array((
-    json_object(
-            'heading', heading,
-            'subheading', subheading
+  db.query(`
+    select substring(link, 2), json_array((
+      json_object(
+        'heading', heading,
+        'subheading', subheading
         )
     ))
-from programs;`, (error, result) => {
-    if (error) throw result;
-    let obj = {};
-    result.map(v => {
-      let dbString = Object.values(v)
-      let key = dbString[0];
-      let value = JSON.parse(dbString[1]);
-      obj[key] = value;
-    })
-    console.log('obj',obj);
-    res.json(obj);
+    from programs;`, (error, result) => {
+      if (error) throw result;
+      let obj = {};
+      result.map(v => {
+        let dbString = Object.values(v)
+        let key = dbString[0];
+        let value = JSON.parse(dbString[1]);
+        obj[key] = value;
+      })
+      // console.log('obj',obj);
+      res.json(obj);
   })
 })
-// router.get('/timelinelist', (req,res) => {
-//   db.query(`select json_object(
-//     substring(link, 2), json_array((
-//       select group_concat(
-//         json_object(
-//           'edu', edu,
-//           'name', name,
-//           'title', title,
-//           'description', description,
-//           'startdate', start_date,
-//           'enddate', end_date
-//         )
-//       )
-//       from steps_timelines
-//       inner join gisus on steps_timelines.gisus_id = gisus.id
-//       inner join programs on programs.id = gisus.programs_id
-//      ))
-//     )
-//     from programs;`, (error, result) => {
-//       if (error) throw result;
-//      result.map((v) => {
-//        let each = JSON.parse(Object.values(v)[0])
-//        console.log('hihi', JSON.parse(Object.values(each)[0][0]))
-//      })
-//      // result.map(v => {
-//      //   console.log("----------------------\n", Object.values(v));
-//      // })
-//     // console.log(a);
-//       // console.log(result)
-//       res.json(result);
-//   })
-// })
+
 router.get('/timelinelist', (req, res) => {
   db.query(`  select
     substring(link, 2) as 'program',
     edu,
     name,
-     title,
-     description,
-    'startdate', start_date,
-    'enddate', end_date
+    description,
+    start_date as 'startdate',
+    end_date as 'enddate'
     from steps_timelines
-      inner join gisus on steps_timelines.gisus_id = gisus.id
-      inner join programs on programs.id = gisus.programs_id;`, (error, result) => {
-    if (error) throw error;
-    console.log(result);
-    res.json(result);
+    inner join gisus on steps_timelines.gisus_id = gisus.id
+    inner join programs on programs.id = gisus.programs_id;`, (error, result) => {
+      if (error) throw error;
+      let obj = {};
+      result.map(v => {
+        //각각의 프로그램이름들('ftseoul'등)이 obj에 없으면 obj['ftseoul']에 빈배열[]을 만듬
+        if (!(v.program in obj)){
+          obj[v.program] = [];
+        }
+        //만들어진 빈배열에 오브젝트 push
+        obj[v.program].push(v);
+        //'ftseoul'같은 프로그램 이름은 obj의 키값으로 적어줬기때문에 v에 있는 'ftseoul'은 삭제
+        delete v['program']
+      })
+      // console.log(obj);
+      res.json(obj);
   })
 })
-
 
 module.exports = router;
 
