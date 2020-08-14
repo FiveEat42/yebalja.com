35648c3465f1964c9145f972e4be2da444404c0e
seongsangCHO <secho@student.42seoul.kr>
2020-08-14 14:03:33 +0900

    Refactor unnecessary code in redux

--- front/redux/actions/faqAction.js
+++ front/redux/actions/faqAction.js
@@ -1,21 +1,10 @@
 import axios from 'axios';
 //액션(type을 가진 객체) 생성함수
 export const getData =  async () => {
+  //API 호출
   const faqData = await axios.get('http://localhost:5000/api/json/faq');
-  console.log('call getData faqAction');
-  //faqData.data =>로 변경해야함
   return {
     type: 'GET_FAQ_DATA',
     payload : faqData.data
-  }
-}
-
-
-export const test = async() => {
-  console.log('call getData faqAction');
-  //faqData.data =>로 변경해야함
-  return {
-    type: 'GET_FAQ_DATA',
-    payload : await axios.get('http://localhost:5000/api/json/faq')
   }
 }
--- front/redux/reducers/faq.js
+++ front/redux/reducers/faq.js
@@ -1,25 +1,17 @@
 import { HYDRATE } from 'next-redux-wrapper';
 
-
 const initialState = {
   data: {}
 };
 
 const faqReducer = (state = initialState, action) => {
-console.log('faqReducer')
-console.log(action.type)
   switch (action.type) {
     case HYDRATE:
       return { ...state, ...action.payload };
 
     case 'GET_FAQ_DATA':
-      console.log('GET_FAQ_DATA');
-      console.log(action.payload)
       return { ...state, data: action.payload };
 
-    case 'TEST_ACTION':
-      console.log('TEST_ACTION')
-      return {...state, data : 'secho'}
     default:
       return state;
   }
