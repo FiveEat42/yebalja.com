dcaf961607cecb80440d55a7163529b753b45c36
365kim <365listener@gmail.com>
2020-11-12 18:32:21 +0900

    Add dummy data for navbar with new pages

--- front/components/common/NavBar.js
+++ front/components/common/NavBar.js
@@ -13,6 +13,62 @@ import classNames from "classnames";
 import Form from "react-bootstrap/Form";
 import { useRouter } from "next/router";
 
+// const navList = {
+//   bootcamp: [
+//     {
+//       program: "42서울",
+//       href: "/ftseoul",
+//     },
+//     {
+//       program: "SSAFY",
+//       href: "/ssafy",
+//     },
+//     {
+//       program: "부스트캠프",
+//       href: "/boostcamp",
+//     },
+//     {
+//       program: "SOMA",
+//       href: "/soma",
+//     },
+//     {
+//       program: "우아한테크코스",
+//       href: "/woowa",
+//     },
+//     {
+//       program: "멋쟁이사자처럼",
+//       href: "/likelion",
+//     },
+//   ],
+//   topstacks: [
+//     {
+//       topic: "언어",
+//       href: "/language",
+//     },
+//     {
+//       topic: "프론트엔드",
+//       href: "/frontend",
+//     },
+//     {
+//       topic: "백엔드",
+//       href: "/backend",
+//     },
+//     ,
+//     {
+//       topic: "DBMS",
+//       href: "/dbms",
+//     },
+//     {
+//       topic: "딥러닝",
+//       href: "/deeplearning",
+//     },
+//     {
+//       topic: "에디터",
+//       href: "/editor",
+//     },
+//   ],
+// };
+
 function NavItem({ data }) {
   const router = useRouter();
 
@@ -135,7 +191,7 @@ export default function NavBar() {
           >
             <Button variant="link" className={styles.buttonPopover}>
               <img
-                src={require("../src/image/Expand.png")}
+                src={require("../../src//image/Expand.png")}
                 width="24"
                 height="24"
               />
