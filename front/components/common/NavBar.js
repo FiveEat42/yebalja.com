b8538bb9ec8f080ab259b83546ea6e4ec329cf2d
365kim <365listener@gmail.com>
2020-11-16 10:49:44 +0900

    Add nav_sub

--- front/components/common/NavBar.js
+++ front/components/common/NavBar.js
@@ -8,71 +8,73 @@ import { useRouter } from "next/router";
 import LogoWhite from "./LogoWhite";
 import RollingNotice from "./RollingNotice";
 
-const navListDummy = {
-  bootcamp: [
-    {
-      title: "한눈에보기",
-      href: "/bootcamp",
-    },
-    {
-      title: "42서울",
-      href: "/ftseoul",
-    },
-    {
-      title: "SSAFY",
-      href: "/ssafy",
-    },
-    {
-      title: "부스트캠프",
-      href: "/boostcamp",
-    },
-    {
-      title: "SOMA",
-      href: "/soma",
-    },
-    {
-      title: "우아한테크코스",
-      href: "/woowa",
-    },
-    {
-      title: "멋쟁이사자처럼",
-      href: "/likelion",
-    },
-  ],
-  topstacks: [
-    {
-      title: "언어",
-      href: "/language",
-    },
-    {
-      title: "프론트엔드",
-      href: "/frontend",
-    },
-    {
-      title: "백엔드",
-      href: "/backend",
-    },
-    ,
-    {
-      title: "DBMS",
-      href: "/dbms",
-    },
-    {
-      title: "딥러닝",
-      href: "/deeplearning",
-    },
-    {
-      title: "에디터",
-      href: "/editor",
-    },
-  ],
-};
+const navListDummy = [
+  { title: "부트캠프",
+    href: "/bootcamp", 
+    sublist : [
+      {
+        title: "42서울",
+        href: "/ftseoul",
+      },
+      {
+        title: "삼성 SSAFY",
+        href: "/ssafy",
+      },
+      {
+        title: "네이버 부스트캠프",
+        href: "/boostcamp",
+      },
+      {
+        title: "SW마에스트로",
+        href: "/soma",
+      },
+      {
+        title: "우아한테크코스",
+        href: "/woowa",
+      },
+      {
+        title: "멋쟁이사자처럼",
+        href: "/likelion",
+      },
+    ],
+  },
+  { title: "탑스택스",
+    href: "/topstacks", 
+    sublist : [
+      {
+        title: "프로그래밍 언어",
+        href: "/language",
+      },
+      {
+        title: "웹 프론트엔드",
+        href: "/frontend",
+      },
+      {
+        title: "웹 백엔드",
+        href: "/backend",
+      },
+      ,
+      {
+        title: "DBMS",
+        href: "/dbms",
+      },
+      {
+        title: "딥러닝",
+        href: "/deeplearning",
+      },
+      {
+        title: "에디터/IDE",
+        href: "/editor",
+      },
+    ]
+  }
+]
 
-function NavItem({ data }) {
+function SubItem({ data }) {
   const router = useRouter();
   return (
     <>
-      <li className={styles.subitem}>
+      <li>
         <Link href={data.href}>
           <a
             className={router.pathname == data.href ? "active" : ""}
@@ -80,6 +82,28 @@ function NavItem({ data }) {
             {data.title}
           </a>
         </Link>
+      </li>
+    </>
+  );
+}
+
+function MainItem({ data }) {
+  const router = useRouter();
+  return (
+    <>
+      <li>
+        <Link href={data.href}>
+          <a
+            className={router.pathname == data.href ? "active" : ""}
+          >
+            {data.title}
+          </a>
+        </Link>
+        <ul className={styles.nav_sub}>
+          {data.sublist?.map((v) => (
+            <SubItem data={v} key={v.title} />
+          ))}
+        </ul>
       </li>
     </>
   );
@@ -111,31 +135,10 @@ export default function NavBar() {
 
         {/* 내비 main */}
         <ul className={styles.nav_main}>
-          <li>
-            <Link href="/">
-              <a>부트캠프</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/">
-              <a>탑스택스</a>
-            </Link>
-          </li>
+          {navListDummy?.map((v) => (
+                <MainItem data={v} key={v.title} />
+              ))}
         </ul>
-
-        {/* 내비 sub */}
-        <div className={styles.nav_sub}>
-          <ul>
-            {navListDummy.bootcamp?.map((v) => (
-              <NavItem data={v} key={v.title} />
-            ))}
-          </ul>
-          <ul>
-            {navListDummy.topstacks?.map((v) => (
-              <NavItem data={v} key={v.title} />
-            ))}
-          </ul>
-        </div>
 
         <div className={styles.toggle}>
           <span></span>
@@ -145,6 +148,7 @@ export default function NavBar() {
         <div className={styles.notice}>
           <RollingNotice />
         </div>
+
       </div>
     </div>
   );
