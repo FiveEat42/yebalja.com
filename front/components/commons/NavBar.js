d7babe443c362cd8262eec7b1eadb0d6efe72c6f
365kim <365listener@gmail.com>
2020-11-19 17:39:57 +0900

    Implement responsive navbar

--- front/components/commons/NavBar.js
+++ front/components/commons/NavBar.js
@@ -1,193 +1,133 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { getNoticeData } from '../../redux/actions/noticeAction';
-import { getNavbarData } from '../../redux/actions/navbarAction';
-import styles from './NavBar.module.css'
-import Link from 'next/link';
+import { getNoticeData } from "../../redux/actions/noticeAction";
+import { getNavbarData } from "../../redux/actions/navbarAction";
+import styles from "./NavBar.module.css";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import OverlayTrigger from "react-bootstrap/OverlayTrigger"
-import Button from "react-bootstrap/Button"
-import Popover from "react-bootstrap/Popover"
-import classNames from 'classnames';
-import Form from 'react-bootstrap/Form';
-import { useRouter } from "next/router";
+import classNames from "classnames";
 
 const navListDummy = [
-   {
-      "title":"어디서 배울까",
-      "sublist":[
-         {
-            "title":"비교하기",
-            "href":"/bootcamps"
-         },
-         {
-            "title":"42SEOUL",
-            "href":"/ftseoul"
-         },
-         {
-            "title":"삼성 SSAFY",
-            "href":"/ssafy"
-         },
-         {
-            "title":"네이버 부스트캠프",
-            "href":"/boostcamp"
-         },
-         {
-            "title":"SW마에스트로",
-            "href":"/soma"
-         },
-         {
-            "title":"우아한테크코스",
-            "href":"/woowa"
-         },
-         {
-            "title":"멋쟁이사자처럼",
-            "href":"/likelion"
-         }
-      ]
-   },
-   {
-      "title":"무엇을 배울까",
-      "sublist":[
-         {
-            "title":"프로그래밍 언어",
-            "href":"/language"
-         },
-         {
-            "title":"웹 프론트엔드",
-            "href":"/frontend"
-         },
-         {
-            "title":"웹 백엔드",
-            "href":"/backend"
-         },
-         {
-            "title":"DBMS",
-            "href":"/dbms"
-         },
-         {
-            "title":"딥러닝",
-            "href":"/deeplearning"
-         },
-         {
-            "title":"에디터/IDE",
-            "href":"/editor"
-         }
-      ]
-   }
-]
+  {
+    title: "어디서 배울까",
+    sublist: [
+      {
+        title: "비교하기",
+        href: "/bootcamps",
+      },
+      {
+        title: "42SEOUL",
+        href: "/ftseoul",
+      },
+      {
+        title: "삼성 SSAFY",
+        href: "/ssafy",
+      },
+      {
+        title: "네이버 부스트캠프",
+        href: "/boostcamp",
+      },
+      {
+        title: "SW마에스트로",
+        href: "/soma",
+      },
+      {
+        title: "우아한테크코스",
+        href: "/woowa",
+      },
+      {
+        title: "멋쟁이사자처럼",
+        href: "/likelion",
+      },
+    ],
+  },
+  {
+    title: "무엇을 배울까",
+    sublist: [
+      {
+        title: "프로그래밍 언어",
+        href: "/language",
+      },
+      {
+        title: "웹 프론트엔드",
+        href: "/frontend",
+      },
+      {
+        title: "웹 백엔드",
+        href: "/backend",
+      },
+      {
+        title: "DBMS",
+        href: "/dbms",
+      },
+      {
+        title: "딥러닝",
+        href: "/deeplearning",
+      },
+      {
+        title: "에디터/IDE",
+        href: "/editor",
+      },
+    ],
+  },
+];
 
-function NavItem ({ data }) {
-
+function NavItem({ data }) {
   const router = useRouter();
 
   return (
-    <>
+    <div className={styles.nav_sub}>
       <Link href={data.href}>
-        <a className={classNames({["nav-link"]: true,
-          [router.pathname == [data.href] ? "active" : ""]: true})}>
-            {data.program}
+        <a
+          className={classNames({
+            ["nav-link"]: true,
+            [router.pathname == [data.href] ? "active" : ""]: true,
+          })}>
+          {data.title}
         </a>
       </Link>
-    </>
-  )
-}
-
-function RollingItem ({ data }) {
-  
-  /* 남은 날짜, 시간에 따라 D-Day 표현방식(status) 결정 */
-  let status;
-  let target = new Date(data.enddate);
-  let today = new Date();
-
-  if (today > target) {
-    status = '마감';
-  } else if (target.setHours(0, 0, 0, 0) === today.setHours(0, 0, 0, 0)) {
-    status = 'D-Day';
-  } else {
-    status= `D-${(target - today)/(60*60*24*1000)}`;
-  }
-  
-  return (
-    <>
-      <li>
-        <a target="_blank" rel="noopener noreferrer" href={data.href}>
-          <span className={styles.date}>{status}</span>
-          <span className={styles.notice}>{data.title}</span>
-        </a>
-      </li>
-    </>
-  )
-}
-
-function ListItem ({ data, idx }) {
-  
-  return (
-    <>
-      <li className={styles.noticePopoverItem}>
-        <a target="_blank" rel="noopener noreferrer" href={data.href}>
-        <span>· {data.title}</span>
-        </a>
-      </li>
-    </>
-  )
+    </div>
+  );
 }
 
 export default function NavBar() {
-
   const navbarData = useSelector((state) => state.navbarReducer.data);
-  const noticeData = useSelector((state) => state.noticeReducer.data);
   const dispatch = useDispatch();
 
   useEffect(() => {
-      getNavbarData().then(function(result){
-        dispatch(result)
-      });
-      getNoticeData().then(function(result){
-        dispatch(result)
-      });
-    }, 
-  []);
+    getNavbarData().then(function (result) {
+      dispatch(result);
+    });
+  }, []);
 
-  const noticeList = noticeData ?? [];
   const navList = navbarData ?? [];
 
   return (
     <div>
-      <Navbar collapseOnSelect expand="lg" variant="dark" className={styles.navbarBg}>
+      <Navbar collapseOnSelect expand="lg" variant="dark" className={styles.nav_background}>
         <Navbar.Brand></Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-
           <Nav className="mr-auto">
-              {navList?.map((v) => <NavItem data={v} key={v.program}/>)}
-          </Nav>
-
-          <Form inline className={styles.noticeRolling}>
             <ul>
-              {noticeList?.map((v) => <RollingItem data={v} key={v.title}/>)}
+              <li className={styles.nav_main}>
+                <h3>{navListDummy[0].title}</h3>
+                {navListDummy[0].sublist?.map((v) => (
+                  <NavItem data={v} key={v.title} />
+                ))}
+              </li>
+              <li className={styles.nav_main}>
+                <h3>{navListDummy[1].title}</h3>
+                {navListDummy[1].sublist?.map((v) => (
+                  <NavItem data={v} key={v.title} />
+                ))}
+              </li>
             </ul>
-          </Form>
-
-          <OverlayTrigger trigger="click" placement="bottom" 
-            overlay={
-              <Popover id={`popover-positioned-bottom`} className={styles.noticePopover}>
-                <Popover.Title className={styles.noticePopoverTitle}>실시간 모집공고</Popover.Title>
-                <Popover.Content className={styles.noticePopoverList}>
-                    <ul id="noticePopoverList">
-                      {noticeList?.map((v, idx) => <ListItem data={v} key={v.title} id={idx}/>)}
-                    </ul> 
-                  </Popover.Content>
-              </Popover>
-            }>
-            <Button variant="link" className={styles.buttonPopover}>
-              <img src={require('../../srcs/commons/Expand.png')} width="24" height="24" />
-            </Button>
-          </OverlayTrigger>
-
+          </Nav>
         </Navbar.Collapse>
       </Navbar>
     </div>
   );
-};
+}
